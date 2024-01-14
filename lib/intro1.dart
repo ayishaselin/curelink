6580f0f5b55403c81a626d3f1ab32b1f3a7b5085
afrahkp <afrahanas12@gmail.com>
2024-01-14 22:34:51 +0530

    introfull

--- lib/intro1.dart
+++ lib/intro1.dart
@@ -5,23 +5,76 @@ class SecondPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+  
     return Scaffold(
        
       body: 
         Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Image.asset('images/doctor1.png'),
-            const SizedBox(height: 20),
-            const  Text(
-              'Your Bottom Text',
+             Container(
+            height: MediaQuery.of(context).size.height * 0.8,
+            child: Image.asset('images/doctorintro.png'),
+          ),
+           const SizedBox(height: 5),
+          Text(
+            'Your Personalized Platform Connecting You to the Perfect Doctor',
+            style: TextStyle(
+              fontSize: 12,
+              color: Color.fromARGB(244, 70, 67, 67),
+              fontWeight: FontWeight.normal,
+            ),
+          ),
+           const SizedBox(height: 10),
+           
+             ElevatedButton(
+              onPressed: () {
+                // Handle button press
+              },
+              child: Text('Get Started'),
+              style: ElevatedButton.styleFrom(
+                padding: const EdgeInsets.all(15),
+                primary: Color.fromARGB(255, 35, 73, 241), // Adjust the background color as needed
+                textStyle: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  
+                ),
+                 shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(40.0), // Rounded edges
+              ),minimumSize: Size(150, 0),
+
+              ),
+            ),
+             const SizedBox(height: 5),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Already have an account?',
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.black,
+                ),
+              ),
+            
+          
+          TextButton(
+            onPressed: () {
+              // Handle "Sign in" text button press
+            },
+            child: Text(
+              'Sign in.',
               style: TextStyle(
-                fontSize: 40,
-                color: Colors.black,
+                fontSize: 14,
+                color: Colors.blue,
                 fontWeight: FontWeight.bold,
-              ),)
-          ],
-        )
+              ),
+            ),
+          ),],
+          )],
+        ),
     );
   }
 }
