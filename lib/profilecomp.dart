63dc887a86b2c4f071717d5c8c473c4097781dda
afrahkp <105631607+afrahkp@users.noreply.github.com>
2024-01-22 22:36:14 +0530

    profile

---
+++ lib/profilecomp.dart
@@ -0,0 +1,170 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Profile extends StatefulWidget {
+  const Profile({super.key});
+
+  @override
+  State<Profile> createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+  
+  TextEditingController nameController = TextEditingController();
+  TextEditingController phoneNumberController = TextEditingController();
+  String? selectedGender;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(
+            child: Text(
+              'Complete your profile',
+              style: GoogleFonts.inter(
+                fontWeight: FontWeight.w500,
+                fontSize: 25.0,
+              ),
+            ),
+          ),
+          SizedBox(height: 10,),
+          Text(
+            'Don’t worry, only you can see your personal',
+            style: GoogleFonts.inter(
+              fontWeight: FontWeight.normal,
+              fontSize: 12.0,
+              color: Color.fromARGB(255, 97, 95, 95),
+            ),
+          ),
+          Text(
+            'data. No one else will be able to see it.',
+            style: GoogleFonts.inter(
+              fontWeight: FontWeight.normal,
+              fontSize: 12.0,
+              color: Color.fromARGB(255, 97, 95, 95),
+            ),
+          ),
+          const SizedBox(height: 200.0),
+          
+           Container(
+            padding: EdgeInsets.symmetric(horizontal: 16.0),
+             child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Name',
+                    style: GoogleFonts.inter(
+                      fontWeight: FontWeight.normal,
+                      fontSize: 16.0,
+                    ),
+                  ),
+                  const SizedBox(height: 7.0),
+                  Container(
+                        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.grey),
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                        child: const TextField(
+                          decoration: InputDecoration(
+                            hintText: 'Enter your name',
+                            hintStyle: TextStyle(color: Colors.grey,fontWeight: FontWeight.w200),
+                            border: InputBorder.none,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 20.0),
+                       Text(
+                  'Phone Number',
+                  style: GoogleFonts.inter(
+                    fontWeight: FontWeight.normal,
+                    fontSize: 16.0,
+                  ),
+                ),
+                const SizedBox(height: 9.0),
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  child: TextField(
+                    controller: phoneNumberController,
+                    keyboardType: TextInputType.phone,
+                    decoration: InputDecoration(
+                      hintText: 'phone number',
+                      hintStyle: TextStyle(color: Colors.grey,fontWeight: FontWeight.w200),
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20.0),
+                       Text(
+                  'Select your gender',
+                  style: GoogleFonts.inter(
+                    fontWeight: FontWeight.normal,
+                    fontSize: 16.0,
+                  ),
+                ),
+                const SizedBox(height: 9.0),
+                  Container(
+                  width: double.infinity,  // Ensure that the container takes up the full width
+
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  child: DropdownButton<String>(
+                    value: selectedGender,
+                    icon: const Icon(Icons.arrow_drop_down),
+                    iconSize: 24,
+                    elevation: 16,
+                    style: const TextStyle(color: Color.fromARGB(255, 90, 88, 88)),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        selectedGender = newValue;
+                      });
+                    },
+                    items: <String>['Male', 'Female', 'Others']
+                        .map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                  ),
+                ),
+                 const SizedBox(height: 40.0),
+                ElevatedButton(
+                onPressed: () {
+                  
+                  // Handle sign-in logic here
+                },
+                child: Text('Complete your profile', style: GoogleFonts.inter(color: Colors.white)),
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.all(15),
+                  backgroundColor: const Color.fromARGB(255, 1, 101, 252),
+                  textStyle: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.normal,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(40.0),
+                  ),
+                  minimumSize: const Size(380, 0),
+                ),
+              ),
+              ],
+      ))],
+        ));
+                      
+           
+          ;  }
+}
--- lib/verify.dart
+++ lib/verify.dart
@@ -1,6 +1,5 @@
- // verification_page.dart
-
-import 'package:flutter/material.dart';
+ import 'package:flutter/material.dart';
+import 'package:flutter_application_1/profilecomp.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class EmailVerificationPage extends StatefulWidget {
@@ -13,70 +12,143 @@ class EmailVerificationPage extends StatefulWidget {
 }
 
 class _EmailVerificationPageState extends State<EmailVerificationPage> {
-  String enteredOtp = '';
+  late List<TextEditingController> otpControllers;
+  late List<FocusNode> otpFocusNodes;
+
+  @override
+  void initState() {
+    super.initState();
+    otpControllers = List.generate(4, (index) => TextEditingController());
+    otpFocusNodes = List.generate(4, (index) => FocusNode());
+  }
+
+  @override
+  void dispose() {
+    for (var controller in otpControllers) {
+      controller.dispose();
+    }
+    for (var focusNode in otpFocusNodes) {
+      focusNode.dispose();
+    }
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-         
-      ),
+      appBar: AppBar(),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
         child: Column(
-        
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Text(
-                'Verify Code',
-                style: GoogleFonts.inter(
-                  fontWeight: FontWeight.w500,
-                  fontSize: 25.0,
-                ),
+              'Verify Code',
+              style: GoogleFonts.inter(
+                fontWeight: FontWeight.w500,
+                fontSize: 25.0,
               ),
-            
-                Text(
-                  'Please enter the code we just sent to your email',
-                   style: GoogleFonts.inter(
-                  fontWeight: FontWeight.normal,
-                  fontSize: 12.0,
-                  color: Colors.grey,
-                ),
-                  
-                ),
-              
+            ),
+            Text(
+              'Please enter the code we just sent to your email',
+              style: GoogleFonts.inter(
+                fontWeight: FontWeight.normal,
+                fontSize: 12.0,
+                color: Colors.grey,
+              ),
+            ),
             Text(
               '${widget.email}',
               style: TextStyle(fontSize: 12),
             ),
-          
-            SizedBox(height: 20),
-            TextFormField(
-              onChanged: (value) {
-                setState(() {
-                  enteredOtp = value;
-                });
-              },
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                labelText: 'Enter OTP',
+            const SizedBox(height: 20),
+
+              Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: List.generate(
+                4,
+                (index) => Container(
+                  margin: EdgeInsets.symmetric(horizontal: 5),
+                  width: 60.0,
+                  child: TextField(
+                    controller: otpControllers[index],
+                    keyboardType: TextInputType.number,
+                    textAlign: TextAlign.center,
+                    maxLength: 1,
+                    style: TextStyle(fontSize: 20),
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.all(15),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(color: Colors.grey),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(color: Colors.blue),
+                      ),
+                      counterText: '',  
+                    ),
+                    onChanged: (value) {
+                      // Move focus to the next TextField when a digit is entered
+                      if (value.isNotEmpty && index < 3) {
+                        FocusScope.of(context).requestFocus(otpFocusNodes[index + 1]);
+                      }
+                    },
+                    focusNode: otpFocusNodes[index],
+                  ),
+                ),
               ),
             ),
-            SizedBox(height: 20),
+            
+            SizedBox(height: 40),
+            Text(
+              'Didn’t receive OTP?',
+              style: GoogleFonts.inter(
+                fontWeight: FontWeight.normal,
+                fontSize: 12.0,
+                color: Colors.grey,
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                // Handle "Resend code" button press
+              },
+              style: TextButton.styleFrom(
+                padding: EdgeInsets.zero,
+                minimumSize: Size.zero,
+                tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+              ),
+              child: Text(
+                'Resend code',
+                style: GoogleFonts.inter(
+                  fontSize: 10,
+                  color: Colors.blue,
+                  fontWeight: FontWeight.normal,
+                ),
+              ),
+            ),
+            SizedBox(height: 60),
             ElevatedButton(
               onPressed: () {
-                // Validate the entered OTP and proceed accordingly
-                // For simplicity, let's assume the correct OTP is '123456'
-                if (enteredOtp == '123456') {
-                  // Navigate to the success page or perform further actions
-                  // For now, just print a message
-                  print('OTP verification successful!');
-                } else {
-                  // Handle incorrect OTP
-                  print('Incorrect OTP. Please try again.');
-                }
+                  Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => Profile()));
+                // Handle verification logic here
+                // You can access the entered OTP using otpControllers[index].text
               },
-              child: Text('Verify OTP'),
+              child: Text('Verify', style: GoogleFonts.inter(color: Colors.white)),
+              style: ElevatedButton.styleFrom(
+                padding: const EdgeInsets.all(15),
+                backgroundColor: const Color.fromARGB(255, 1, 101, 252),
+                textStyle: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.normal,
+                ),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(40.0),
+                ),
+                minimumSize: const Size(380, 0),
+              ),
             ),
           ],
         ),
