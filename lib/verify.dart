f36c8ac0866445653342c8fbc151ce577740365e
afrahkp <105631607+afrahkp@users.noreply.github.com>
2024-01-25 11:16:24 +0530

    Ismail

--- lib/verify.dart
+++ lib/verify.dart
@@ -1,7 +1,7 @@
  import 'package:flutter/material.dart';
 import 'package:flutter_application_1/profilecomp.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:firebase_core/firebase_core.dart';
+
 
 class EmailVerificationPage extends StatefulWidget {
   final String email;
